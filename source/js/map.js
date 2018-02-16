849a876563bff2d23c2d9e2b40a58ff06de91b4f
denmetall <denmetall@yandex.ru>
2018-02-16 20:01:35 +0700

    Добавляет маркер для интерактивной карты

--- source/js/map.js
+++ source/js/map.js
@@ -1,11 +1,16 @@
-var map;
-
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {
-      lat: -34.397,
-      lng: 150.644
-    },
-    zoom: 8
+  var uluru = {
+    lat: 59.93877121663107,
+    lng: 30.323274149999975
+  };
+  var image = (screen.width < 768) ? "img/map-pin-small.png" : "img/map-pin.png";
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+    icon: image
   });
 }
